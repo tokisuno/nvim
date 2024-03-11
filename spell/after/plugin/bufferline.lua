82ad452bc054fd6ab99ec74bdf5098c2f556fb2f
tokisuno <lucaswmei@gmail.com>
2024-03-10 20:01:19 -0400

    removed spell from repo

--- spell/after/plugin/bufferline.lua
+++
@@ -1,9 +0,0 @@
-require("bufferline").setup{
-    options = {
-        themable = true,
-        numbers = "ordinal",
-        indicator = "underline",
-        separator_style = "slope",
-        diagnostics = "nvim_lsp"
-    }
-}
