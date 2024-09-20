ff6d149c33c32faeb868899a0d7d4566b8c09b9d
tokisuno <lucaswmei@gmail.com>
2024-09-20 17:39:54 -0400

    oops

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -8,12 +8,4 @@ return {
     version = '0.1.5',
   },
   {"nvim-telescope/telescope-bibtex.nvim"},
-  {"nvim-telescope/telescope-ui-select.nvim"},
-  {"nvim-telescope/telescope-frecency.nvim"},
-  {"nvim-telescope/telescope-cheat.nvim",
-    dependencies = {
-      "kkharji/sqlite.lua",
-      "nvim-telescope/telescope.nvim"
-    }
-  }
 }
